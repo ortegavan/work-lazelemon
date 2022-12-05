9e8085cb682e5c8490376da20b55a5e15ea610a3
Van Ortega <ortegavan@live.com>
2022-12-05 16:50:32 -0300

    [chore] responsividade

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ function gtag() {
 }
 gtag('js', new Date());
 gtag('config', 'UA-134237019-3');
+
+document.querySelector('.mobile-open').addEventListener('click', () => {
+	document.querySelector('nav').classList.add('show');
+	document.querySelector('.social').classList.add('show');
+	document.querySelector('.mobile-close').classList.add('show');
+});
+
+document.querySelector('.mobile-close').addEventListener('click', () => {
+	document.querySelector('nav').classList.remove('show');
+	document.querySelector('.social').classList.remove('show');
+	document.querySelector('.mobile-close').classList.remove('show');
+});
